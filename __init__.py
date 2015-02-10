70a695098b053288015518fb5d85ebce9142cbfe
mwas <francismwangi152@gmail.com>
2015-02-10 11:41:03 +0300

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+__author__ = 'mwas'
+__created_date__ = '2/10/15'
+__email___ = 'francismwangi152@gmail.com'
+__phone_number__ = '+254702729654'
+
+
+import NotFound
+
+not_found = NotFound.NotFound()
